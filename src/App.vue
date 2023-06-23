6db70f03cc96c3733583d287c615b1d65ceaf9d3
grenzk <74033991+grenzk@users.noreply.github.com>
2023-06-23 18:03:41 +0800

    Style input element

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+</script>
 
 <template>
   <main>
@@ -10,6 +13,11 @@
 
       <div class="user-input">
         <h1 class="title">IP Address Tracker</h1>
+
+        <div class="p-inputgroup">
+          <InputText placeholder="Search for any IP address or domain" />
+          <Button icon="pi pi-chevron-right" severity="secondary" />
+        </div>
       </div>
     </div>
   </main>
@@ -22,25 +30,45 @@
   img {
     width: 100%;
   }
+}
 
-  .user-input {
-    position: absolute;
+.user-input {
+  position: absolute;
 
-    top: 1.7rem;
-    left: 50%;
-    width: 100%;
-    transform: translateX(-50%);
+  top: 1.7rem;
+  left: 50%;
+  width: 88%;
+  transform: translateX(-50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 2rem;
+
+  .title {
+    color: var(--neutral-white);
+    font-size: var(--biggest-font-size);
+    font-weight: var(--font-medium);
+    line-height: 1.2;
+  }
+
+  .p-inputgroup {
+    --button-border-radius: 0.9rem;
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    row-gap: 2rem;
+    width: 100%;
 
-    .title {
-      color: var(--neutral-white);
-      font-size: var(--biggest-font-size);
-      font-weight: var(--font-medium);
-      line-height: 1.2;
+    .p-inputtext {
+      border-radius: var(--button-border-radius) 0 0 var(--button-border-radius);
+      padding: 1rem;
+      width: 100%;
+      font-size: var(--small-font-size);
+    }
+
+    .p-button {
+      background-color: var(--primary-black);
+      border-color: var(--primary-very-dark-gray);
+      border-radius: 0 var(--button-border-radius) var(--button-border-radius) 0;
+      flex-basis: 20%;
     }
   }
 }
