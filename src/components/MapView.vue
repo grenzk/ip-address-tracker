a759c2f96791c76b476205419114c5d88d782cf5
grenzk <74033991+grenzk@users.noreply.github.com>
2023-07-05 16:09:10 +0800

    Calculathe the remaining height for the map container

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,26 +1,42 @@
 <script setup>
-import { ref } from 'vue'
-import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+import { ref, onMounted } from 'vue'
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
 
 let zoom = ref(12)
-let center = ref([40.6782, -73.9442])
+let center = ref([35.6895, 139.6917])
+let markerCoordinates = ref([35.6895, 139.6917])
+const mapHeight = ref('100%')
+
+const adjustMapHeight = () => {
+  const containerElement = document.querySelector('.container')
+  const windowHeight = window.innerHeight
+
+  const containerHeight = (containerElement.offsetHeight / windowHeight) * 100
+
+  mapHeight.value = `${100 - containerHeight}%`
+}
+
+onMounted(() => {
+  adjustMapHeight()
+  window.addEventListener('resize', adjustMapHeight)
+})
 </script>
 
 <template>
-  <div class="map">
-    <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
-      <l-tile-layer
+  <div class="map" :style="{ height: mapHeight }">
+    <LMap :zoom="zoom" :center="center" :useGlobalLeaflet="false">
+      <LTileLayer
         url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
         layer-type="base"
         name="Stadia Maps Basemap"
-      ></l-tile-layer>
-    </l-map>
+      ></LTileLayer>
+      <LMarker :lat-lng="markerCoordinates"></LMarker>
+    </LMap>
   </div>
 </template>
 
 <style scoped lang="scss">
 .map {
-  height: 100%;
   width: 100%;
   position: absolute;
   bottom: 0;
